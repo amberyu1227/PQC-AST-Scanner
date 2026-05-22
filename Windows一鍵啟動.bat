a20d83d9d608d665c8647644dc34905f7e5b0aa5
Vitas1205 <vitas453535@gmail.com>
2026-05-23 00:47:10 +0800

    Add files via upload

--- Windows一鍵啟動.bat
+++ Windows一鍵啟動.bat
@@ -1,9 +1,3 @@
 @echo off
-title PQC Hybrid Auditor
-echo ===================================================
-echo   正在啟動 PQC 混合自動化審計系統 (Windows 綠色版)...
-echo ===================================================
 cd /d "%~dp0"
-
-:: 呼叫封包內的 Python 執行 Streamlit
-env\python.exe -m streamlit run src\website.py --server.port 8501
+venv\Scripts\python -m streamlit run src\website.py --server.port 8501
